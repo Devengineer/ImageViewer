9e8c00ad37943b2e7fb2f9e65fca0f3872cdc8a6
Devengineer <waldemar.yan@gmail.com>
2016-02-16 11:59:45 +0600

    Initial check-in of module imageViewer

---
+++ imageViewer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gtk;
+
+namespace imageViewer
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
